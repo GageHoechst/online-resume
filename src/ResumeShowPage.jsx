1a89730f9ae801986dabce2b33b6790bbfd8d621
alexisxaltamirano <alexisaltamirano1998@gmail.com>
2024-04-16 12:16:53 -0700

    fixed showpage

--- src/ResumeShowPage.jsx
+++ src/ResumeShowPage.jsx
@@ -1,12 +1,14 @@
+/* eslint-disable react/prop-types */
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
-export function ResumeShowPage() {
+export function ResumeShowPage(props) {
+  console.log(props);
   const [resume, setResume] = useState({});
   const params = useParams();
 
-  const handleShowResume = (props) => {
+  const handleShowResume = () => {
     // axios.get(`http://localhost:3000/resumes/${params.id}.json`).then((response) => {
     //   setResume(response.data);
     // });
@@ -17,9 +19,16 @@ export function ResumeShowPage() {
   return (
     <div id="resumes-show">
       <h1>Resume Info</h1>
-      <p>First: {resume.firstName}</p>
-      <p>Last: {resume.lastName}</p>
-      <p>Short Bio: {resume.shortBio}</p>
+      <p>First: {props.resume.firstName}</p>
+      <p>Last: {props.resume.lastName}</p>
+      <p>Email: {props.resume.email}</p>
+      <p>Phone Number: {props.resume.phoneNumber}</p>
+      <p>Short Bio: {props.resume.shortBio}</p>
+      <p>LinkedIn: {props.resume.linkedInURL}</p>
+      <p>Twitter: {props.resume.twitterHandle}</p>
+      <p>Personal Blog: {props.resume.personalBlogURL}</p>
+      <p>Online: {props.resume.onlineResumeURL}</p>
+      <p>Github: {props.resume.githubURL}</p>
     </div>
   );
 }
